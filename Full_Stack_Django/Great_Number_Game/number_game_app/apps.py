ff5ace8bdccaf12a06751675757fb2a10cb35391
Reem310 <106007879+Reem310@users.noreply.github.com>
2022-06-18 23:37:42 +0300

    Delete Full_Stack_Django/Great_Number_Game directory

--- Full_Stack_Django/Great_Number_Game/number_game_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NumberGameAppConfig(AppConfig):
-    name = 'number_game_app'
--- Full_Stack_Django/Great_Number_Game/number_game_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path     
-from . import views
-urlpatterns = [
-    path('', views.index),	
-    path('guess', views.guess),	
-    path('again', views.clear),	
-]
