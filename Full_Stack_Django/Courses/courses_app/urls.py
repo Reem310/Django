05fa732c667a160a5dcb2c9d73c4d873d8867a93
Reem310 <106007879+Reem310@users.noreply.github.com>
2022-06-20 02:12:21 +0300

    Delete Full_Stack_Django/Courses directory

--- Full_Stack_Django/Courses/courses_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path     
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('add', views.add_course),
-    path('courses/destroy/<_id>', views.delete_option),
-    path('destroy/<_id>', views.destroy),
-]
