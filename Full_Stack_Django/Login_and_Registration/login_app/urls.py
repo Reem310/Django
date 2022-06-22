64da4935886a1415cc954df9add3892ab5ff965c
Reem310 <106007879+Reem310@users.noreply.github.com>
2022-06-22 03:15:37 +0300

    Delete Full_Stack_Django/Login_and_Registration directory

--- Full_Stack_Django/Login_and_Registration/login_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path     
-from . import views
-urlpatterns = [
-    path('', views.index),	
-    path('reg', views.register),
-    path('login', views.login),
-    path('success', views.success),
-    path('destroy', views.destroy),
-]
