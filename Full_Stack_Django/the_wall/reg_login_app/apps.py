3c1fb4fd8ed6a808d524c48db83a5946397ab9b1
Reem310 <106007879+Reem310@users.noreply.github.com>
2022-06-22 15:13:46 +0300

    Delete Full_Stack_Django/the_wall directory

--- Full_Stack_Django/the_wall/reg_login_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegLoginAppConfig(AppConfig):
-    name = 'reg_login_app'
--- Full_Stack_Django/the_wall/the_wall_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TheWallAppConfig(AppConfig):
-    name = 'the_wall_app'
